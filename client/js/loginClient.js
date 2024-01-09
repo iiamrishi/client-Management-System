fac3c118e50cc7e0977a6be221dfcfc8751f4226
rishi <rishirajbabu123@gmail.com>
2024-01-09 20:57:49 +0530

    fixed the sign in alignment

--- client/js/loginClient.js
+++ client/js/loginClient.js
@@ -2,39 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailInput = document.getElementById("emailInput");
   const passwordInput = document.getElementById("passwordInput");
   const togglePassword = document.getElementById("togglePassword");
-  const emailLabel = document.querySelector('label[for="emailInput"]');
-  const passwordLabel = document.querySelector('label[for="passwordInput"]');
-  let passwordVisible = false;
 
   // Toggle password visibility on click
   togglePassword.addEventListener("click", function () {
-    passwordVisible = !passwordVisible;
-
-    const type = passwordVisible ? "text" : "password";
-    passwordInput.setAttribute("type", type);
-
-    // Toggle between "visibility" and "visibility_off" icons
-    togglePassword.textContent = passwordVisible
-      ? "visibility_off"
-      : "visibility";
+    // Toggle logic to show/hide password
   });
 
-  // Hide the labels when there is any input in the respective fields
+  // Function to handle input events
+  function handleInputEvent(inputElement) {
+    const icon = inputElement.nextElementSibling;
+
+    // Show or hide the eye icon based on input value
+    icon.style.display = inputElement.value.length > 0 ? "block" : "none";
+  }
+
+  // Add input event listeners
   emailInput.addEventListener("input", function () {
-    emailLabel.style.display = emailInput.value.length > 0 ? "none" : "block";
+    handleInputEvent(emailInput);
   });
 
   passwordInput.addEventListener("input", function () {
-    passwordLabel.style.display =
-      passwordInput.value.length > 0 ? "none" : "block";
+    handleInputEvent(passwordInput);
   });
 
   // Hide the eye icon initially
   togglePassword.style.display = "none";
-
-  // Show the eye icon when typing a password
-  passwordInput.addEventListener("input", function () {
-    togglePassword.style.display =
-      passwordInput.value.length > 0 ? "block" : "none";
-  });
 });
